a52ec20d5700852a159c740a8ecc6b92d13387e1
Lendl Peralta <janlendl@gmail.com>
2021-11-06 16:39:21 -0600

    Add conditionals to only show what is needed based from the wins

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,9 @@ function Player(props) {
   return (
     <article className='Player'>
       <h1>{ props.firstName } <span>{ props.gamerTag }</span> { props.lastName }</h1>
-      {/* To be shown when there are no wins */}<h2 className='zero'>Currently with no wins</h2>
-      {/* To be shown when there is 1 win */}<h2>Currently at 1 win</h2>
-      {/* To be shown when there is more than 1 win */}<h2>Currently at {props.wins} wins</h2>
+        {props.wins === 0 && <h2 className="zero">Currently with no wins 😞</h2>}
+        {props.wins === 1 && <h2>Currently at {props.wins} win 🙂</h2>}
+        {props.wins > 1 && <h2>Currently at {props.wins} wins 😎</h2>}
     </article>
   )
 }
