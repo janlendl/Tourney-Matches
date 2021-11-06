034024953c3478aae265dbdb7bfa7c5dc683144e
Lendl Peralta <janlendl@gmail.com>
2021-11-06 12:30:05 -0600

    Add playerData and matchData in import list

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,6 +1,9 @@
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
 import Player from './Player';
 
-function PlayerList() {
+
+function PlayerList(props) {
   return (
     <section className='PlayerList'>
       <h1>Current participating players</h1>
