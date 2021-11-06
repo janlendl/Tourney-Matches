eceecdf976c9eb9a77032ade9df79306808cc04d
Lendl Peralta <janlendl@gmail.com>
2021-11-06 11:39:14 -0600

    Insert Match component

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,8 +1,11 @@
+import Match from './Match';
+
 function MatchList() {
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
       {/* Matches will be shown here */}
+      <Match />
   </section>
   )
 }
