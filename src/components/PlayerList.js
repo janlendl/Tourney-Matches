4c4f441a71457a63c9a59c414e813de0c08d9e3d
Lendl Peralta <janlendl@gmail.com>
2021-11-07 14:01:53 -0700

    Clean up code and remove imported data and replaced it as props

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,14 +1,9 @@
-import matchData from '../data/matchData';
-import playerData from '../data/playerData';
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
 import Player from './Player';
 
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = props.parsedPlayerData[0];
 
-  const onePlayer = parsedPlayerData[0];
   return (
     <section className='PlayerList'>
       <h1>Current participating players</h1>
